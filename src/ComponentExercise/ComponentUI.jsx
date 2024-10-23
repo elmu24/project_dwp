1599e608688e025102f0f2fc37964534f29cd42a
elmu24 <elena.mueller@muebu.eu>
2024-10-23 13:45:31 +0300

    creating Header component - filling in ComponentUI

---
+++ src/ComponentExercise/ComponentUI.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Header from './Header';
+import OrderForm from './OrderForm';
+import OrderInfo from './OrderInfo';
+
+
+// Arrays for product names and prices
+const productName = ["AMD Ryzen 5 3600", "AMD Ryzen 5 3500", "AMD Ryzen 3 3300"];
+const productPrice = [245, 215, 195];
+
+function ComponentUI() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [quantity, setQuantity] = useState(1);
+
+  const handleProductChange = (index) => {
+    setSelectedIndex(index);
+  };
+
+  const handleQuantityChange = (newQuantity) => {
+    if (newQuantity >= 0) {
+      setQuantity(newQuantity);
+    }
+  };
+
+  return (
+    <div className="content">
+      <Header title="WELCOME PRODUCT PAGE!" image="https://picsum.photos/100" altText="some random picture" />
+      
+    </div>
+  );
+}
+
+export default ComponentUI;
